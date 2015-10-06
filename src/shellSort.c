c7a1a960819320bb161397c3f4497bbb95a8a6d1
Liu Yik Cheng <liuyikcheng@msn.com>
2015-10-07 00:14:48 +0800

    Created shellSort functions and severeal test functions

--- src/shellSort.c
+++ src/shellSort.c
@@ -3,15 +3,30 @@
 #include <stdio.h>
 
 
-int *shellSort(int arr[], int numOfElem){
-	int firstValue, secondValue, gap, num, i;
-	i=sizeof(arr);
+int *shellSort(int *arr, int numOfElem){
+	int firstValue, secondValue, gap, i, j, value;
+	i = 0;
+	j = 0;
+	gap = numOfElem;
 	
+
+	do{
+	gap = gap / 2;
+	for(i = gap;i <= (numOfElem-1);i++){
+	j = i - gap;
+	value = arr[i];
 	
-	int a = sizeof(int);
+		while((arr[j] > value)&&(j >= 0)){
+			arr[j+gap] = arr[j];
+			j = j - gap;
+		}
+		arr[j+gap] = value;
+	}
+	}while(gap!=1);
 	
-	printf("b%db", numOfElem);
-	
+	for(i=0; i<numOfElem; i++){
+	printf("%d ",arr[i]);
+	}
 	
 	return arr;
 }
--- test/test_shellSort.c
+++ test/test_shellSort.c
@@ -1,8 +1,5 @@
 #include "unity.h"
 #include "shellSort.h"
-#include "addList.c"
-#include "linkedlist.c"
-#include "listElement.c"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -10,9 +7,85 @@ void setUp(void){}
 
 void tearDown(void){}
 
-void test_array(void){
+void test_passing_array(void){
 	int arr[] = {1,2,4,6} ;
+	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
-	shellSort(arr, numOfElem);
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(1, sortedArr[0]);
+	TEST_ASSERT_EQUAL(2, sortedArr[1]);
+	TEST_ASSERT_EQUAL(4, sortedArr[2]);
+	TEST_ASSERT_EQUAL(6, sortedArr[3]);
+}
+
+void test_sorting_two_array_elements(void){
+	int arr[] = {3,2};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(2, sortedArr[0]);
+	TEST_ASSERT_EQUAL(3, sortedArr[1]);
+}
+
+void test_sorting_three_array_elements(void){
+	int arr[] = {3,2,1};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(1, sortedArr[0]);
+	TEST_ASSERT_EQUAL(2, sortedArr[1]);
+	TEST_ASSERT_EQUAL(3, sortedArr[2]);
+}
+
+void test_sorting_five_array_elements(void){
+	int arr[] = {7,82,1,5,21};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(1, sortedArr[0]);
+	TEST_ASSERT_EQUAL(5, sortedArr[1]);
+	TEST_ASSERT_EQUAL(7, sortedArr[2]);
+	TEST_ASSERT_EQUAL(21, sortedArr[3]);
+	TEST_ASSERT_EQUAL(82, sortedArr[4]);
+}
+
+void test_sorting_ten_array_elements(void){
+	int arr[] = {3,82,1,5,21,9,50,77,3334,10};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(1, sortedArr[0]);
+	TEST_ASSERT_EQUAL(3, sortedArr[1]);
+	TEST_ASSERT_EQUAL(5, sortedArr[2]);
+	TEST_ASSERT_EQUAL(9, sortedArr[3]);
+	TEST_ASSERT_EQUAL(10, sortedArr[4]);
+	TEST_ASSERT_EQUAL(21, sortedArr[5]);
+	TEST_ASSERT_EQUAL(50, sortedArr[6]);
+	TEST_ASSERT_EQUAL(77, sortedArr[7]);
+	TEST_ASSERT_EQUAL(82, sortedArr[8]);
+	TEST_ASSERT_EQUAL(3334, sortedArr[9]);
+}
+
+void test_sorting_thee_array_elements_with_one_negative_element(void){
+	int arr[] = {3,82,9,-4};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(-4, sortedArr[0]);
+	TEST_ASSERT_EQUAL(3, sortedArr[1]);
+	TEST_ASSERT_EQUAL(9, sortedArr[2]);
+	TEST_ASSERT_EQUAL(82, sortedArr[3]);
 	
 }
