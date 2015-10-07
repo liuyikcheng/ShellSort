54bf3a3c0212a217d342a4c486f404aee59caac4
finux <finux2500@hotmail.com>
2015-10-07 18:25:58 +0800

    Edited test with all negative elements

--- src/shellSort.c
+++ src/shellSort.c
@@ -11,20 +11,21 @@ int *shellSort(int *arr, int numOfElem){
 	
 
 	do{
-	gap = gap / 2;
-	for(i = gap;i <= (numOfElem-1);i++){
-	j = i - gap;
-	value = arr[i];
+    gap = gap/2;
+    
+    for(i = gap; i <= (numOfElem-1); i++){
+      j = i-gap;
+      value = arr[i];
 	
-		while((arr[j] > value)&&(j >= 0)){
-			arr[j+gap] = arr[j];
-			j = j - gap;
-		}
-		arr[j+gap] = value;
-	}
-	}while(gap!=1);
+      while((arr[j] > value)&&(j >= 0)){
+        arr[j+gap] = arr[j];
+        j = j-gap;
+      }
+      arr[j+gap] = value;
+    }
+	}while(gap != 1);
 	
-	for(i=0; i<numOfElem; i++){
+	for(i = 0; i < numOfElem; i++){
 	printf("%d ",arr[i]);
 	}
 	
--- test/test_shellSort.c
+++ test/test_shellSort.c
@@ -7,6 +7,12 @@ void setUp(void){}
 
 void tearDown(void){}
 
+
+/**
+ *    Check soreted array:
+ *        1,2,4,6
+ *
+ */
 void test_passing_array(void){
 	int arr[] = {1,2,4,6} ;
 	int *sortedArr;
@@ -20,7 +26,13 @@ void test_passing_array(void){
 	TEST_ASSERT_EQUAL(6, sortedArr[3]);
 }
 
-void test_sorting_two_array_elements(void){
+
+/**
+ *    Sort array:
+ *        3,2 => 2,3
+ *
+ */
+void test_sorting_two_elements(void){
 	int arr[] = {3,2};
 	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
@@ -31,7 +43,12 @@ void test_sorting_two_array_elements(void){
 	TEST_ASSERT_EQUAL(3, sortedArr[1]);
 }
 
-void test_sorting_three_array_elements(void){
+/**
+ *    Sort array:
+ *        3,2,1 => 1,2,3
+ *
+ */
+void test_sorting_three_elements(void){
 	int arr[] = {3,2,1};
 	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
@@ -43,7 +60,12 @@ void test_sorting_three_array_elements(void){
 	TEST_ASSERT_EQUAL(3, sortedArr[2]);
 }
 
-void test_sorting_five_array_elements(void){
+/**
+ *    Sort array:
+ *        7,82,1,5,21 => 1,5,7,21,82
+ *
+ */
+void test_sorting_five_elements(void){
 	int arr[] = {7,82,1,5,21};
 	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
@@ -57,7 +79,12 @@ void test_sorting_five_array_elements(void){
 	TEST_ASSERT_EQUAL(82, sortedArr[4]);
 }
 
-void test_sorting_ten_array_elements(void){
+/**
+ *    Sort array:
+ *       3,82,1,5,21,9,50,77,3334,10 => 1,3,5,9,10,21,50,77,82,3334
+ *
+ */
+void test_sorting_ten_elements(void){
 	int arr[] = {3,82,1,5,21,9,50,77,3334,10};
 	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
@@ -76,16 +103,41 @@ void test_sorting_ten_array_elements(void){
 	TEST_ASSERT_EQUAL(3334, sortedArr[9]);
 }
 
-void test_sorting_thee_array_elements_with_one_negative_element(void){
+/**
+ *    Sort array:
+ *       3,82,9,-4 => -4,3,9,82
+ *
+ */
+void test_sorting_three_elements_with_one_negative_element(void){
 	int arr[] = {3,82,9,-4};
 	int *sortedArr;
 	int numOfElem = sizeof(arr)/4;
 	
 	sortedArr = shellSort(arr, numOfElem);
-	
+  
 	TEST_ASSERT_EQUAL(-4, sortedArr[0]);
 	TEST_ASSERT_EQUAL(3, sortedArr[1]);
 	TEST_ASSERT_EQUAL(9, sortedArr[2]);
 	TEST_ASSERT_EQUAL(82, sortedArr[3]);
+  
+}
+
+/**
+ *    Sort array:
+ *       3,82,9,-4 => -4,3,9,82
+ *
+ */
+void test_sorting_five_negative_elements(void){
+	int arr[] = {-41,-100,-11,-5,-84};
+	int *sortedArr;
+	int numOfElem = sizeof(arr)/4;
+	
+	sortedArr = shellSort(arr, numOfElem);
+	
+	TEST_ASSERT_EQUAL(-100, sortedArr[0]);
+	TEST_ASSERT_EQUAL(-84, sortedArr[1]);
+	TEST_ASSERT_EQUAL(-41, sortedArr[2]);
+	TEST_ASSERT_EQUAL(-11, sortedArr[3]);
+	TEST_ASSERT_EQUAL(-5, sortedArr[4]);
 	
 }
